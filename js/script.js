d625eeb535051e7ec3f0b679e0d3a8030ae954b2
Kristina <kr.hranovska@gmail.com>
2023-09-25 16:21:35 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// for mobile-close-menu
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-mobile-open]"),
+    closeModalBtn: document.querySelector("[data-mobile-close]"),
+    modal: document.querySelector("[data-mobile]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
+
+
+// for button-open-modal
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
